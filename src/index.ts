c2143af73286f0d988cbd90cbfda1ee9eb9cfdee
Uri Shaked <uri@urishaked.com>
2019-01-11 00:33:18 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { tsquery } from '@phenomnomnominal/tsquery';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { createInterface } from 'readline';
+import { FunctionDeclaration } from 'typescript';
+import { createGunzip } from 'zlib';
+
+interface IInputRecord {
+  id: string;
+  paths: string[];
+  line: string;
+  character: string;
+  text: string;
+}
+
+function trimBody(body: string) {
+  // Remove beginning/final curly braces
+  return body.substr(1, body.length - 1).trim();
+}
+
+const input = createReadStream(join(__dirname, '../data/typescript-all-functions.json.gz')).pipe(createGunzip());
+createInterface({ input }).on('line', (entry) => {
+  const parsedRecord = JSON.parse(entry) as IInputRecord;
+  const ast = tsquery.ast(parsedRecord.text);
+  const fnNode = tsquery.query<FunctionDeclaration>(ast, 'FunctionDeclaration')[0];
+  if (!fnNode.body) {
+    // empty function?
+    return;
+  }
+  const name = fnNode.name ? fnNode.name.text : ''; // empty = default function
+  const args = fnNode.parameters;
+  const body = trimBody(fnNode.body!.getText());
+  console.log({
+    name,
+    argCount: args.length,
+    argNames: args.map((n) => n.name.getText()),
+    body,
+  });
+});
